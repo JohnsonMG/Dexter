eb6abb2de14877bdd6c53c72850004db367dbf93
Andrew Chin <chin.andrew@outlook.com>
2017-03-20 19:10:33 -0400

    Use servo library instead of analogWrite for compatibility reasons

--- InputTest/InputTest.ino
+++ InputTest/InputTest.ino
@@ -1,32 +1,15 @@
 /*
-  Analog Input
- Demonstrates analog input by reading an analog sensor on analog pin 0 and
- turning on and off a light emitting diode(LED)  connected to digital pin 13.
- The amount of time the LED will be on and off depends on
- the value obtained by analogRead().
+Dexter Movement
+*/
 
- The circuit:
- * Potentiometer attached to analog input 0
- * center pin of the potentiometer to the analog pin
- * one side pin (either one) to ground
- * the other side pin to +5V
- * LED anode (long leg) attached to digital output 13
- * LED cathode (short leg) attached to ground
+#include <Servo.h>
 
- * Note: because most Arduinos have a built-in LED attached
- to pin 13 on the board, the LED is optional.
+Servo servo_fl;
+Servo servo_fr;
+Servo servo_bl;
+Servo servo_br;
 
-
- Created by David Cuartielles
- modified 30 Aug 2011
- By Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/AnalogInput
-
- */
- const int HOME = 512;
+const int HOME = 512;
 
 int joyX = 0;
 int joyY = 1;
@@ -43,12 +26,17 @@ int dead_min = 485;
 int dead_max = 520;
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
   Serial.begin(9600);
-  pinMode(pin_fl, OUTPUT);
-  pinMode(pin_fr, OUTPUT);
-  pinMode(pin_bl, OUTPUT);
-  pinMode(pin_br, OUTPUT);
+
+  servo_fl.attach(pin_fl);
+  servo_fr.attach(pin_fr);
+  servo_bl.attach(pin_bl);
+  servo_br.attach(pin_br);
+
+//  pinMode(pin_fl, OUTPUT);
+//  pinMode(pin_fr, OUTPUT);
+//  pinMode(pin_bl, OUTPUT);
+//  pinMode(pin_br, OUTPUT);
 }
 
 void loop() {
@@ -56,70 +44,79 @@ void loop() {
   set_motors();
 }
 
-void read_input(){
+void send_motors() {
+  //send motor values to output pins
+
+  Serial.print("motors[]= ");
+  Serial.print(motors[0]);
+  Serial.print(", ");
+  Serial.print(motors[1]);
+  Serial.print(", ");
+  Serial.print(motors[2]);
+  Serial.print(", ");
+  Serial.println(motors[3]);
+
+  servo_fl.write(motors[0]);
+  servo_fr.write(motors[1]);
+  servo_bl.write(motors[2]);
+  servo_br.write(motors[3]);
+
+//  analogWrite(pin_fl, motors[0]);
+//  analogWrite(pin_fr, motors[1]);
+//  analogWrite(pin_bl, motors[2]);
+//  analogWrite(pin_fr, motors[3]);
+}
+
+void read_input() {
   // read the value from the sensor:
   int x = analogRead(joyX);
   int y = analogRead(joyY);
-  int r = HOME; //analogRead(joyR);
-  
+  int r = 768; //analogRead(joyR);
+
   //nullify deadzone
   valX = (x >= dead_max || x <= dead_min) ? x : HOME;
   valY = (y >= dead_max || y <= dead_min) ? y : HOME;
   valR = (r >= dead_max || r <= dead_min) ? r : HOME;
   shift_inputs();
-  
+
   //testing print
   Serial.print("X: ");
   Serial.println(valX);
   Serial.print("Y: ");
   Serial.println(valY);
-  
+
   //control polling frequency
   delay(500);
 }
 
 //Adjust input range from 0 <-> 1023 to -512 <-> 512
-void shift_inputs(){
+void shift_inputs() {
   valX = valX - HOME;
   valY = valY - HOME;
   valR = valR - HOME;
 }
 
-void set_motors(){
+void set_motors() {
   //logic to set motor value. Adding HOME shifts range back to 0-1024
   motors[0] = valY + valX + valR + HOME; //front left
   motors[1] = valY - valX - valR + HOME; //front right
   motors[2] = valY - valX + valR + HOME; //back left
   motors[3] = valY + valX - valR + HOME; //back right
-  
+
   scale_outputs();
-  
+
   send_motors();
 }
 
-void scale_outputs(){
-  //ensure range of 0 to 1023 then divide for pwm range of 0-255
-  for(int i=0; i<4; i++){
-      if(motors[i] > 1023) motors[i] = 1023;
-      if(motors[i] < 0) motors[i] = 0;
-      motors[i] = motors[i]/4;
+void scale_outputs() {
+  //ensure range of 0 to 1023 then map for servo input range of 0-180
+  for (int i = 0; i < 4; i++) {
+    if (motors[i] > 1023) motors[i] = 1023;
+    if (motors[i] < 0) motors[i] = 0;
+
+    motors[i] = (int)map(motors[i], 0, 1023, 0, 180);
   }
 }
 
-void send_motors(){
- //send motor values to output pins
- 
- Serial.print("motors[]= ");
- Serial.print(motors[0]);
- Serial.print(", ");
- Serial.print(motors[1]);
- Serial.print(", ");
- Serial.print(motors[2]);
- Serial.print(", ");
- Serial.println(motors[3]);
- 
- analogWrite(pin_fl, motors[0]); 
- analogWrite(pin_fr, motors[1]);
- analogWrite(pin_bl, motors[2]);
- analogWrite(pin_fr, motors[3]);
-}
+
+
